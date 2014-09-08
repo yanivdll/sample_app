a5a596e7c95fc0f0c638dbe548061f6319ba6d02
Yaniv Gilad <yanivdll@gmail.com>
2014-09-07 23:52:36 -0400

    run through chapter 7 (till 7.3 - failed signup).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
-  def new
-  end
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			#Handle a successful save
+		else
+			render 'new'
+			binding.pry
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
 end
