6fbfa072a6c245455d9909a9262014b7cf8074a3
yanivdll <yanivdll@gmail.com>
2014-10-24 23:41:37 -0400

    added delete user functionality

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,10 +1,12 @@
 namespace :db do 
 	desc "fill in database with sample users"
 	task populate: :environment do
-		User.create!(name: "Example User",
+		admin = User.create!(name: "Example User",
 			email: "example@example.com",
 			password: "foobar",
 			password_confirmation: "foobar")
+		admin.toggle!(:admin)
+		
 		99.times do
 			name = Faker::Name.name
 			email = Faker::Internet.email #"example-#{n+1}@example.com"
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -22,6 +22,25 @@ describe "User Pages" do
 				page.should have_selector('li', text: user.name)
 			end
 		end
+
+		describe "delete links" do
+			it {should_not have_link('delete')}
+
+			describe "as an admin" do
+				let(:admin) {FactoryGirl.create(:admin)}
+
+				before do
+					sign_in admin
+					visit users_path
+				end
+
+				it {should have_link('delete', href: user_path(User.first))}
+				it "should be able to delete a user who isn't himself" do
+					expect {click_link('delete')}.to change(User, :count).by(-1)
+				end
+				it {should_not have_link('delete', href: user_path(admin))}
+			end
+		end
 	end
 
 	describe "Profile Page" do
