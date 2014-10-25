337079ab6ed03d65e0e50b7cb8e6aba82c1f708c
yanivdll <yanivdll@gmail.com>
2014-10-24 21:30:32 -0400

    added adminitrator indication to the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,9 +10,11 @@ describe User do
   it {should respond_to(:password_digest)}
   it {should respond_to(:password)}
   it {should respond_to(:password_confirmation)}
+  it {should respond_to(:admin)}
   it {should respond_to(:authenticate)}
   it {should respond_to(:remember_token)}
   it {should be_valid}
+  it {should_not be_admin}
 
   describe "when name is not present" do
     before {@user.name = ""}
@@ -109,6 +111,12 @@ describe User do
   describe "remember token" do
     before {@user.save}
     its(:remember_token) {should_not be_blank}
+  end
+
+  describe "whith admin attribute set to true" do 
+    before {@user.toggle!(:admin)}
+
+    it {should be_admin}
   end
 end
 
